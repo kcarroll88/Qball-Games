bf66f126a51e949baee6bbfce5dfb1dc382d1483
Keenan <keenan.carroll.va@gmail.com>
2019-07-30 12:44:52 -0400

    level loader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,5 +15,21 @@ public class SceneLoader : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SplashScene()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(SplashScreenTime());
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+
+    IEnumerator SplashScreenTime()
+    {
+        yield return new WaitForSeconds(3f);
     }
 }
