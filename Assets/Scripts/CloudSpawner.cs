46898857fb84dd2f7f10332f89e7ad875b3a2ba5
Keenan <keenan.carroll.va@gmail.com>
2019-07-31 13:59:18 -0400

    got clouds spawing and flying

---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField] Clouds[] clouds;
+    [SerializeField] float minSpawnSpeed;
+    [SerializeField] float maxSpawnSpeed;
+
+    bool spawning = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawning)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnSpeed, maxSpawnSpeed));
+            SpawnCloud();
+        }
+    }
+
+    private void SpawnCloud()
+    {
+        var cloudIndex = UnityEngine.Random.Range(0, clouds.Length);
+        Spawn(clouds[cloudIndex]);
+    }
+
+    private void StopSpawningCloud()
+    {
+        spawning = false;
+    }
+
+    private void Spawn(Clouds myCloud)
+    {
+        Clouds newCloud = Instantiate(myCloud, transform.position, Quaternion.identity) as Clouds;
+        newCloud.transform.parent = transform;
+    }
+
+}
