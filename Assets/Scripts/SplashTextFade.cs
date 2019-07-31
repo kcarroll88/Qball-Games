c04f1febdb9495a5bb3bb95655b6c13e84b0a792
Keenan <keenan.carroll.va@gmail.com>
2019-07-31 12:01:01 -0400

    working on fading text

---
+++ Assets/Scripts/SplashTextFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SplashTextFade : MonoBehaviour
+{
+    //Fade time in seconds
+    [SerializeField] public float fadeOutTime;
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    private IEnumerator FadeOutRoutine()
+    {
+        Text text = GetComponent<Text>();
+        Color originalColor = text.color;
+
+        for (float t = 0.01f; t < fadeOutTime; t += Time.deltaTime)
+        {
+            text.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, t / fadeOutTime));
+            yield return null;
+        }
+    }
+}
