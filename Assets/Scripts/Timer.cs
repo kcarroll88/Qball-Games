3965c9467a02aaf580b2eaae55b81ca20aa33a62
Keenan <keenan.carroll.va@gmail.com>
2019-07-31 14:36:30 -0400

    timer built

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] float countdownTime;
+    [SerializeField] Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerText.text = countdownTime.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Countdown();
+    }
+
+    private void Countdown()
+    {
+        countdownTime -= Time.deltaTime;
+        if (countdownTime >= 1)
+        {
+            timerText.text = countdownTime.ToString("F0");
+        }
     }
 }
