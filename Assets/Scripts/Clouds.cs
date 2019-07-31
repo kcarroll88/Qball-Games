508e48b036d901a253135b34ed809706500ebed7
Keenan <keenan.carroll.va@gmail.com>
2019-07-31 13:27:51 -0400

    cloud movement

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    Rigidbody2D myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveClouds();
+    }
+
+    private void MoveClouds()
+    {
+        transform.Translate (Vector2.right * moveSpeed * Time.deltaTime);
+    }
+}
