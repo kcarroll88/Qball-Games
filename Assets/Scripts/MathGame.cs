9f0b07dedb33ba9d239c83e8d2508f8bb4bcd6f0
Keenan <keenan.carroll.va@gmail.com>
2019-08-01 13:13:36 -0400

    math game code

---
+++ Assets/Scripts/MathGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MathGame : MonoBehaviour
+{
+    [SerializeField] int minNumber;
+    [SerializeField] int maxNumber;
+    [SerializeField] Text mathProblemText;
+
+    [SerializeField] bool playing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateMathProblem();
+    }
+
+    public void CreateMathProblem()
+    {
+        while (playing)
+        {
+            var number1 = UnityEngine.Random.Range(minNumber, maxNumber);
+            var number2 = UnityEngine.Random.Range(minNumber, maxNumber);
+            var mathProblem = number1 + number2;
+
+            mathProblemText.text = mathProblem.ToString();
+            return;
+        }
+    }
+}
